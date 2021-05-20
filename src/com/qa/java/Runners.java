c920f98d7d5a42081d053610fb5dbc3ac66c7ca4
Corry-Coder <59158851+Corry-Coder@users.noreply.github.com>
2021-05-20 01:58:25 -0400

    CW: Adding Calculator changes

--- src/com/qa/java/Runners.java
+++ src/com/qa/java/Runners.java
@@ -3,7 +3,10 @@ package com.qa.java;
 public class Runners {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int result = Calculator.add(5, 5);
+		
+		System.out.println(result);
 
 	}
 
